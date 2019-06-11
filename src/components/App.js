d194ad07ba8b576d0271bf8767f8c569292ee52b
shaktigurung <mr.shakti.gurung@gmail.com>
2019-06-12 09:33:27 +1000

    css on draggable and droppable

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,32 @@ import './../App.css';
 import initialData from '../data/initial-data';
 import Column from './Column';
 import {DragDropContext} from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const Container = styled.div `
+  display: flex;
+`;
 
 class App extends React.Component {
   state = initialData;
 
+  onDragStart = () => {
+    document.body.style.color ='orange';
+    document.body.style.transition = 'background-color 0.2s ease';
+
+  }
+
+  onDragUpdate = update => {
+    const {destination} = update;
+    const opacity = destination 
+      ? destination.index / Object.keys(this.state.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153, 141,217, ${opacity})`;
+  };
+
   onDragEnd = result => {
+    document.body.style.color ='inherit';
+    document.body.style.backgroundColor ='inherit';
     const {destination, source, draggableId} = result;
 
     if(!destination) {
@@ -20,35 +41,72 @@ class App extends React.Component {
       return;
     }
 
-    const column = this.state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
 
-    const newColumn = {
-      ...column,
-      taskIds: newTaskIds
+    if(start === finish)
+    {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds
+      };
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn
+        }
+      }
+      this.setState(newState);
+      return;
+    }
+    //Moving from one list to another
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds
     };
 
     const newState = {
       ...this.state,
       columns: {
         ...this.state.columns,
-        [newColumn.id]: newColumn
-      }
-    }
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish
+      },
+    };
     this.setState(newState);
-  }
+  };
+
   render(){
 
   return (
-    <DragDropContext onDragEnd= {this.onDragEnd}>
-    {this.state.columnOrder.map((columnId) => {
-        const column = this.state.columns[columnId];
-        const tasks = column.taskIds.map((taskId)=> this.state.tasks[taskId]);
+    <DragDropContext 
+      onDragStart={this.onDragStart}
+      onDragUpdate={this.onDragUpdate}
+      onDragEnd= {this.onDragEnd}
+    >
+      <Container>
+        {this.state.columnOrder.map((columnId) => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map((taskId)=> this.state.tasks[taskId]);
 
-        return <Column key={column.id} column ={column} tasks={tasks} />
-      })}
+            return <Column key={column.id} column ={column} tasks={tasks} />
+          })}
+        </Container>
     </DragDropContext>
    );
   }
--- src/components/Task.js
+++ src/components/Task.js
@@ -7,20 +7,42 @@ const Container = styled.div `
     border-radius: 2px;
     padding: 8px;
     margin-bottom: 8px;
-    background-color: ${props => (props.isDragging ? 'lightgreen': 'white')};
+    background-color: ${props => 
+     props.isDragDisabled 
+        ? 'lightgrey'
+        : props.isDragging 
+            ?'lightgreen'
+            :'white' };
+    display: flex;
+`;
+
+const Handle = styled.div `
+    width: 20px;
+    height: 20px;
+    background-color: orange;
+    border-radius: 4px;
+    margin-right: 8px;
 `;
 
 class Task extends Component {
     render(){
+        const isDragDisabled = this.props.task.id === 'task-1';
+        console.log(isDragDisabled);
         return (
-            <Draggable draggableId={this.props.task.id} index={this.props.index}>
+            <Draggable 
+                draggableId={this.props.task.id} 
+                index={this.props.index}
+                isDragDisabled={isDragDisabled}
+            >
                 { (provided, snapshot )=> (
                 <Container
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                     ref = {provided.innerRef}
                     isDragging={snapshot.isDragging}
+                    isDragDisabled={isDragDisabled}
                 > 
+                    <Handle />
                     {this.props.task.content} 
                 </Container>
                 )}
