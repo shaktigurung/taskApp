238c49577f1af288e190546d32adf9344c82f6f6
shaktigurung <mr.shakti.gurung@gmail.com>
2019-06-11 15:25:45 +1000

    React D n D integrated

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './../App.css';
+import initialData from '../data/initial-data';
+import Column from './Column';
+import {DragDropContext} from 'react-beautiful-dnd';
+
+class App extends React.Component {
+  state = initialData;
+
+  onDragEnd = result => {
+    
+  }
+  render(){
+
+  return (
+    <DragDropContext onDragEnd= {this.onDragEnd}>
+    {this.state.columnOrder.map((columnId) => {
+        const column = this.state.columns[columnId];
+        const tasks = column.taskIds.map((taskId)=> this.state.tasks[taskId]);
+
+        return <Column key={column.id} column ={column} tasks={tasks} />
+      })}
+    </DragDropContext>
+   );
+  }
+}
+
+export default App;
